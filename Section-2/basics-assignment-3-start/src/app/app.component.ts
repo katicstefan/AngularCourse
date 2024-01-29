5a964e1fef591a68ebf9fc746160dec9af9d0198
Stefan Katic <katicstefan.coder@gmail.com>
2024-01-29 17:42:52 +0100

    finished assignment 3

--- Section-2/basics-assignment-3-start/src/app/app.component.ts
+++ Section-2/basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  enableDisplayDetailsButton = true;
+  buttonClicks: Array<{time: number, status: boolean}> = [];
+
+  onClickButtonDisplayDetails(event) {
+    this.enableDisplayDetailsButton = !this.enableDisplayDetailsButton;
+    this.buttonClicks.push({time: event.timeStamp, status: this.enableDisplayDetailsButton});
+  }
 }
