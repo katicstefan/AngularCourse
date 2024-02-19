ce243ea8c942390a77a8a7ffaa2fa00aa7db0ce1
Stefan Katic <katicstefan.coder@gmail.com>
2024-02-19 04:22:14 +0100

    section-10 finished

---
+++ Section-10/course-project-services-start/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,29 @@
+import { RecipeService } from './../recipe.service';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+// import { ShoppingListService } from '../../shopping-list/shopping-list.service';
+// import { Ingredient } from '../../shared/ingredient.model';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.css']
+})
+export class RecipeDetailComponent implements OnInit {
+  @Input() recipe: Recipe;
+
+  // constructor(private slService: ShoppingListService) { }
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit() {
+  }
+
+  // onAddToShoppingList(ingredients: Ingredient[]) {
+  //   this.slService.addIngredients(ingredients);
+  // }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+}
---
+++ Section-10/course-project-services-start/src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,24 @@
+import { ShoppingListService } from './shopping-list.service';
+import { Component, OnInit } from '@angular/core';
+
+import { Ingredient } from '../shared/ingredient.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[];
+
+  constructor(private shoppingListService: ShoppingListService) { }
+
+  ngOnInit() {
+    this.ingredients = this.shoppingListService.getIgredients();
+    this.shoppingListService.ingredientsChanged.subscribe(
+      (ingredients: Ingredient[]) => {
+        this.ingredients = ingredients;
+      }
+    )
+  }
+}
