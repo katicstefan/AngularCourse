0407fa1c2cbb96ebe028d8815d29e683b1ac7ed5
Stefan Katic <katicstefan.coder@gmail.com>
2024-02-29 07:11:31 +0100

    section 12 finished

---
+++ Section-12/course-project-routing-start/src/app/recipes/recipe.service.ts
@@ -0,0 +1,43 @@
+import { EventEmitter, Injectable } from "@angular/core";
+
+import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+
+@Injectable()
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Chicken Hamburger',
+      'Very tasty hamburger!',
+      'https://i2.pickpik.com/photos/734/452/849/burger-food-junk-hamburger-preview.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Buns', 2)
+      ]),
+    new Recipe(
+      'French Fries',
+      'French fries with yummy ketchup made from cherry tomatoes!',
+      'https://thissillygirlskitchen.com/wp-content/uploads/2020/02/homemade-french-fries-5-1076x1536.jpg',
+      [
+        new Ingredient('Potato', 2),
+        new Ingredient('Cherry Tomato', 10)
+      ])
+  ];
+
+  constructor(private slService: ShoppingListService) {}
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+}
